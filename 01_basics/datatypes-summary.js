61166d62c06b12954328939c0f297be90be1c587
varshneyharsh314-droid <varshneyharsh314@gmail.com>
2025-09-30 21:33:43 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,10 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+----------------------------------------------------------------
+
+
+stack (primitive) , Heap (Non - primitive)
+
