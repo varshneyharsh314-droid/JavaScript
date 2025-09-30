7f810222b93a837670ac281f342bd2876c45a5bb
varshneyharsh314-droid <varshneyharsh314@gmail.com>
2025-09-30 21:58:46 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,17 +6,19 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
-
+// when you run this command on the console of the browser then inside prototype you give the some methods that are used in strings
+// string is stored in key-value pairs
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
-// console.log(gameName.toUpperCase());
+// console.log(gameName.toUpperCase());// note that the original value of string is not changed
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4)// 4 not included
+newString = gameName.substring(-8, 4) // It ignores the negative value and start from 0
 console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
@@ -32,4 +34,4 @@ console.log(url.replace('%20', '-'))
 
 console.log(url.includes('sundar'))
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-')); // give the array according to the sepeartor
