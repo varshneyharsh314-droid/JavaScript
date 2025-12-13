41449f8d503ddf4b430c08c2a5d44e7ca5f07347
varshneyharsh314-droid <varshneyharsh314@gmail.com>
2025-12-13 21:46:39 +0530

    Add person objects and console.table examples
    Added examples of objects and console.table usage.

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -20,3 +20,45 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+const person1 = {
+  Name: "Rahul",
+  Email: "rahul@gmail.com",
+  City: "Delhi"
+};
+
+const person2 = {
+  Name: "Amit",
+  Email: "amit@gmail.com",
+  City: "Mumbai"
+};
+
+console.table([person1, person2]);
+
+output:
+
+(index)  Name    Email             City
+0        Rahul   rahul@gmail.com   Delhi
+1        Amit    amit@gmail.com    Mumbai
+
+
+console.table({
+  Rahul: {
+    Email: "rahul@gmail.com",
+    City: "Delhi",
+    Age: 22
+  },
+  Amit: {
+    Email: "amit@gmail.com",
+    City: "Mumbai",
+    Age: 23
+  }
+});
+
+
+Output :
+
+(index)   Rahul             Amit
+Email     rahul@gmail.com   amit@gmail.com
+City      Delhi             Mumbai
+Age       22                23
